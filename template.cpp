adc56923d9c3b710602cbe2942647e06353bc9a7
Priyanshkin <priyanshbajpai8960@gmail.com>
2025-03-27 00:27:43 +0530

    lecture 2 program

--- template.cpp
+++ template.cpp
@@ -3,21 +3,39 @@ using namespace std;
 template<class T>
 class vector{
 public:
-T data1;
+T arr1[3];
+T arr2[3];
+T size;
 
-public:
 vector(T a ){
-    this->data1=a;
-    
+    this->size=a;
+
 }
-void show(){
-    cout<<this->data1<<endl;
+T show(){ 
+    T d;
+    for (int i = 0; i <size; i++)
+    {
+        /* code */  d+=this->arr1[i]*arr2[i];
+ 
+    }
+    return d;
+    
 }
 
 };
 int main(){
-vector<char>obj('c');
-obj.show();
+vector<float>obj(3);
+
+obj.arr1[0]=1.0;
+obj.arr1[1]=2.0;
+obj.arr1[2]=3.0;
+
+obj.arr2[0]=4.1;
+obj.arr2[1]=5.0;
+obj.arr2[2]=6.0;
+cout<<obj.show();
+
+
 return 0;
 
 }
