e7f1b562403a59da8cbe840c3561da156348c486
Priyanshkin <priyanshbajpai8960@gmail.com>
2025-03-31 20:26:03 +0530

    checking of palindrome programm

---
+++ palindrome.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n=121;
+   int t=n;
+    int r;
+    int f;
+
+for(int i=0;i<=3;i++)
+{
+  if(n>0){
+r=n%10;
+n=n/10;
+cout<<r<<endl;
+  }
+ /* else if(n==0){
+    {
+        cout<<f;
+    }*/
+  }
+   return 0;
+  }
+ 
+
+
+
+
